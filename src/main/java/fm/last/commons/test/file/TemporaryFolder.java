97d83f6faa6da4500402ca27a565f49559734322
cmathiesen <cdm57@bath.ac.uk>
2019-09-04 14:18:54 +0100

    Update license years

--- src/main/java/fm/last/commons/test/file/TemporaryFolder.java
+++ src/main/java/fm/last/commons/test/file/TemporaryFolder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Last.fm
+ * Copyright 2012-2019 Last.fm
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
